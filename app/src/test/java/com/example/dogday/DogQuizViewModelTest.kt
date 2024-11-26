21926903397ca8f56a15d269cef7b281473835a1
Joachim Christiansen <joachim.l.christiansen@gmail.com>
2024-11-26 12:59:33 +0100

    ny test

--- app/src/test/java/com/example/dogday/DogQuizViewModelTest.kt
+++ app/src/test/java/com/example/dogday/DogQuizViewModelTest.kt
@@ -1,6 +1,5 @@
 package com.example.dogday.viewmodel
 
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.example.dogday.models.DogRecommendation
 import com.example.dogday.models.DogID
 import com.example.dogday.repository.DogRecommendationRepository
@@ -8,13 +7,11 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.TestScope
-import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
 import org.junit.After
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.whenever
