74d923d3ce54e6e58542fbdf65108f0fbc4bafde
Hansen-Sebastian <sebasth@hiof.no>
2024-11-26 19:14:28 +0100

    Added calendar
    Taken from: https://medium.com/@kiwi47/create-a-flexible-and-customizable-calendar-view-in-android-with-jetpack-compose-56dfb911c2ab

--- app/src/main/java/com/example/dogday/screens/HomeScreen.kt
+++ app/src/main/java/com/example/dogday/screens/HomeScreen.kt
@@ -4,16 +4,23 @@ import DogListViewModel
 import android.annotation.SuppressLint
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -22,6 +29,8 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -29,12 +38,20 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.Placeable
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.example.dogday.R
-
+import okhttp3.internal.toImmutableList
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
 
 
 @SuppressLint("StateFlowValueCalledInComposition")
@@ -42,6 +59,15 @@ import com.example.dogday.R
 fun HomeScreen(navController: NavHostController) {
     val viewModel: DogListViewModel = viewModel()
     val numOfDogs = viewModel.numberOfDogs()
+
+    var currentMonth = Date()
+    val calendar = Calendar.getInstance()
+
+    calendar.time = currentMonth
+    val year = calendar.get(Calendar.YEAR)
+    val month = calendar.get(Calendar.MONTH)
+
+    val dates = getMonthDates(year, month)
 
 
     Box(
@@ -63,6 +89,17 @@ fun HomeScreen(navController: NavHostController) {
                 DogCard(navController = navController, numOfDogs)
             }
             HikeCard(navController = navController)
+            CalendarView(
+                month = currentMonth,
+                date = dates,
+                displayNext = false,
+                displayPrev = false,
+                onClickNext = { /*TODO*/ },
+                onClickPrev = { /*TODO*/ },
+                onClick = { clickedDate -> println("Clicked date: $clickedDate") },
+                startFromSunday = true
+            )
+
 
         }
 
@@ -225,7 +262,208 @@ fun BrowseBreedsCard(navController: NavHostController) {
 }
 
 
+//Kalender vi har hentet fra: https://medium.com/@kiwi47/create-a-flexible-and-customizable-calendar-view-in-android-with-jetpack-compose-56dfb911c2ab
 
+private fun Date.formatToCalendarDay(): String = SimpleDateFormat("d", Locale.getDefault()).format(this)
+
+@Composable
+private fun CalendarCell(
+    date: Date,
+    signal: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    val text = date.formatToCalendarDay()
+    Box(
+        modifier = modifier
+            .aspectRatio(1f)
+            .fillMaxSize()
+            .padding(2.dp)
+            .background(
+                shape = RoundedCornerShape(CornerSize(8.dp)),
+                color = colorScheme.secondaryContainer,
+            )
+            .clip(RoundedCornerShape(CornerSize(8.dp)))
+            .clickable(onClick = onClick)
+    ) {
+        if (signal) {
+            Box(
+                modifier = Modifier
+                    .aspectRatio(1f)
+                    .fillMaxSize()
+                    .padding(8.dp)
+                    .background(
+                        shape = CircleShape,
+                        color = colorScheme.tertiaryContainer.copy(alpha = 0.7f)
+                    )
+            )
+        }
+        Text(
+            text = text,
+            color = colorScheme.onSecondaryContainer,
+            modifier = Modifier.align(Alignment.Center)
+        )
+    }
+}
+
+private fun Int.getDayOfWeek3Letters(): String? = Calendar.getInstance().apply {
+    set(Calendar.DAY_OF_WEEK, this@getDayOfWeek3Letters)
+}.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.getDefault())
+
+@Composable
+private fun WeekdayCell(weekday: Int, modifier: Modifier = Modifier) {
+    val text = weekday.getDayOfWeek3Letters()
+    Box(
+        modifier = modifier
+            .aspectRatio(1f)
+            .fillMaxSize()
+    ) {
+        Text(
+            text = text.orEmpty(),
+            color = colorScheme.onPrimaryContainer,
+            modifier = Modifier.align(Alignment.BottomCenter)
+        )
+    }
+}
+
+private fun Date.formatToWeekDay(): Int {
+    val calendar = Calendar.getInstance()
+    calendar.time = this
+    return calendar.get(Calendar.DAY_OF_WEEK)
+}
+
+
+@Composable
+private fun CalendarGrid(
+    date: List<Pair<Date, Boolean>>,
+    onClick: (Date) -> Unit,
+    startFromSunday: Boolean,
+    modifier: Modifier = Modifier,
+) {
+    val weekdayFirstDay = date.first().first.formatToWeekDay()
+    val weekdays = getWeekDays(startFromSunday)
+    CalendarCustomLayout(modifier = modifier) {
+        weekdays.forEach {
+            WeekdayCell(weekday = it)
+        }
+        // Adds Spacers to align the first day of the month to the correct weekday
+        repeat(if (!startFromSunday) weekdayFirstDay - 2 else weekdayFirstDay - 1) {
+            Spacer(modifier = Modifier)
+        }
+        date.forEach {
+            CalendarCell(date = it.first, signal = it.second, onClick = { onClick(it.first) })
+        }
+    }
+}
+
+fun getWeekDays(startFromSunday: Boolean): List<Int> {
+    val lista = (1..7).toList()
+    return (if (startFromSunday) lista else lista.drop(1) + lista.take(1)).toImmutableList()
+}
+
+@Composable
+private fun CalendarCustomLayout(
+    modifier: Modifier = Modifier,
+    horizontalGapDp: Dp = 2.dp,
+    verticalGapDp: Dp = 2.dp,
+    content: @Composable () -> Unit,
+) {
+    val horizontalGap = with(LocalDensity.current) {
+        horizontalGapDp.roundToPx()
+    }
+    val verticalGap = with(LocalDensity.current) {
+        verticalGapDp.roundToPx()
+    }
+    Layout(
+        content = content,
+        modifier = modifier,
+    ) { measurables, constraints ->
+        val totalWidthWithoutGap = constraints.maxWidth - (horizontalGap * 6)
+        val singleWidth = totalWidthWithoutGap / 7
+
+        val xPos: MutableList<Int> = mutableListOf()
+        val yPos: MutableList<Int> = mutableListOf()
+        var currentX = 0
+        var currentY = 0
+        measurables.forEach { _ ->
+            xPos.add(currentX)
+            yPos.add(currentY)
+            if (currentX + singleWidth + horizontalGap > totalWidthWithoutGap) {
+                currentX = 0
+                currentY += singleWidth + verticalGap
+            } else {
+                currentX += singleWidth + horizontalGap
+            }
+        }
+
+        val placeables: List<Placeable> = measurables.map { measurable ->
+            measurable.measure(constraints.copy(maxHeight = singleWidth, maxWidth = singleWidth))
+        }
+
+        layout(
+            width = constraints.maxWidth,
+            height = currentY + singleWidth + verticalGap,
+        ) {
+            placeables.forEachIndexed { index, placeable ->
+                placeable.placeRelative(
+                    x = xPos[index],
+                    y = yPos[index],
+                )
+            }
+        }
+    }
+}
+
+fun getMonthDates(year: Int, month: Int): List<Pair<Date, Boolean>> {
+    val calendar = Calendar.getInstance()
+    calendar.set(year, month, 1)
+    val daysInMonth = calendar.getActualMaximum(Calendar.DAY_OF_MONTH)
+
+    return (1..daysInMonth).map {
+        calendar.set(Calendar.DAY_OF_MONTH, it)
+        Pair(calendar.time, false)
+    }.toImmutableList()
+}
+
+
+
+@Composable
+fun CalendarView(
+    month: Date,
+    date: List<Pair<Date, Boolean>>,
+    displayNext: Boolean,
+    displayPrev: Boolean,
+    onClickNext: () -> Unit,
+    onClickPrev: () -> Unit,
+    onClick: (Date) -> Unit,
+    startFromSunday: Boolean,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier) {
+        Box(modifier = Modifier.fillMaxWidth()) {
+            Text(
+                text = month.formatToMonthString(),
+                style = typography.headlineMedium,
+                color = colorScheme.onPrimaryContainer,
+                modifier = Modifier.align(Alignment.Center),
+            )
+        }
+        Spacer(modifier = Modifier.size(16.dp))
+        if (!date.isNullOrEmpty()) {
+            CalendarGrid(
+                date = date,
+                onClick = onClick,
+                startFromSunday = startFromSunday,
+                modifier = Modifier
+                    .wrapContentHeight()
+                    .padding(horizontal = 16.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
+        }
+    }
+}
+
+fun Date.formatToMonthString(): String = SimpleDateFormat("MMMM", Locale.getDefault()).format(this)
 
 
 
